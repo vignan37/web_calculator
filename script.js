6ee23a4c65f6a1b5454cfcedd9fab7252c88f766
Vignan Kurme <vignan37@gmail.com>
2022-12-22 20:10:09 +0000

    inprogress [vig]

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+var output = document.getElementById("display");
+var buttons = document.getElementsByClassName("button");
+var operand1 = 0;
+var operand2= null;
+var operator = null;
 
+for(var i=0;i<buttons.length;i++){
+    buttons[i].addEventListener('click',logic);
+}
+
+function logic(){
+    var value = this.getAttribute('data-value');
+    if(value=='+'){
+        operator = '+';
+        operand1=parseFloat(display.textContent);
+        display.innerText = '';
+    }
+    else if(value=='='){
+        operand2=parseFloat(display.textContent);
+    }
+    else{
+        display.innerText +=value;
+    }
+}
