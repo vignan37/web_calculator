c1e96625fbf83b78825fb08797fd59b8097032ff
Vignan Kurme <vignan37@gmail.com>
2022-12-23 12:01:32 +0000

    changes in logic, updated switch case [vig]

--- script.js
+++ script.js
@@ -10,18 +10,41 @@ for(var i=0;i<buttons.length;i++){
 
 function logic(){
     var value = this.getAttribute('data-value');
-    if(value=='+'){
-        operator = '+';
-        operand1=parseFloat(display.textContent);
-        display.innerText='';
-    }
-    else if(value=='='){
-        operand2=parseFloat(display.textContent);
-        var result = eval(operand1+ ' ' + operator + ' ' + operand2);
-        display.innerText=result;
+    switch(value){
+        case 'AC':
+            display.innerText='';
+            operand1=0;
+            operand2=null;
+            operator=null;
+            break;
+        case '+':
+            operator = '+';
+            operand1=parseFloat(display.textContent);
+            display.innerText='';
+            break;
+        case '-':
+            operator = '-';
+            operand1=parseFloat(display.textContent);
+            display.innerText='';
+            break;
+        case '/':
+            operator = '/';
+            operand1=parseFloat(display.textContent);
+            display.innerText='';
+            break;
+        case '%':
+            operator = '%';
+            operand1=parseFloat(display.textContent);
+            display.innerText='';
+            break;
+        case '=':
+            operand2=parseFloat(display.textContent);
+            var result = eval(operand1+ ' ' + operator + ' ' + operand2);
+            display.innerText=result;
+            break;
+        default :
+            display.innerText +=value;
 
     }
-    else{
-        display.innerText +=value;
-    }
+
 }
